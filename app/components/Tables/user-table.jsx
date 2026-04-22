af9840da3ac00434a89e6111fbcc52903e6920d5
LeonardoJohnAndrew <leonardojohnandrew@gmail.com>
2026-04-22 13:04:45 +0800

    l

--- app/components/Tables/user-table.jsx
+++ app/components/Tables/user-table.jsx
@@ -3,9 +3,8 @@ import {FaFilter} from "react-icons/fa"
 const UserTable = (props) => {
   return (
     <div className='w-full'>
-        {JSON.stringify(props.data || {})}
        <table className='border border-gray-300 w-full'>
-          <thead className='bg-black text-white border-3 border-darkRed sticky top-0 z-10'>
+          <thead className='bg-black text-white border-3 text-left border-darkRed sticky top-0 z-10'>
               <tr>
            {props.tableHeader.map((header ,index) => ( 
             <th key={index} className="border-b border-gray-300 px-4 py-2 text-sm font-bold">
@@ -26,14 +25,14 @@ const UserTable = (props) => {
                      <td className="px-4 py-2">
                         <img src={(!item.profile_pic || item.profile_pic === null || item.profile_pic === "")? "/profile/Generic avatar.png" : item.profile_pic} className="h-10 w-10"   alt="avatar" />
                      </td>
-                     <td>{item.lastname}</td>
-                     <td>{item.firstname}</td>
-                     <td>{item.middle}</td>
-                     <td>{item.department}</td>
-                     <td>{item.position}</td>
-                     <td>{item.role}</td>
-                     <td>{item.status}</td>
-                     <td>Action</td>
+                     <td className="px-4 py-2">{item.lastname}</td>
+                     <td className="px-4 py-2">{item.firstname}</td>
+                     <td className="px-4 py-2">{item.middle}</td>
+                     <td className="px-4 py-2">{item.department}</td>
+                     <td className="px-4 py-2">{item.position}</td>
+                     <td className="px-4 py-2">{item.role}</td>
+                     <td className="px-4 py-2">{item.status}</td>
+                     <td className="px-4 py-2">Action</td>
                 </tr>
               ))}
           </tbody>
